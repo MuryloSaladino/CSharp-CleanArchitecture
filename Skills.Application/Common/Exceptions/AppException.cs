3d65aeaed005e45ffb2a421ed3e0cf9fe95a4408
MuryloSaladino <murylosaladino@gmail.com>
2024-09-23 15:50:18 -0300

    app validation

---
+++ Skills.Application/Common/Exceptions/AppException.cs
@@ -0,0 +1,6 @@
+namespace Skills.Application.Common.Exceptions;
+
+public class AppException(string message, int statusCode) : Exception(message)
+{
+    public int StatusCode { get; set; } = statusCode;
+}
