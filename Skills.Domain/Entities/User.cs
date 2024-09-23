a487fbc46acc45b59df901869289f59fe4839728
MuryloSaladino <murylosaladino@gmail.com>
2024-09-23 15:01:36 -0300

    initial structure

---
+++ Skills.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace Skills.Domain.Entities;
+
+using Common;
+
+public class User : BaseEntity
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
