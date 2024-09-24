63fa9c17c4ca0d7b75b429b611fd15ae24dc5c05
MuryloSaladino <murylosaladino@gmail.com>
2024-09-24 10:17:07 -0300

    dependency for hasher

---
+++ Skills.API/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Skills.Application.Features.Users.Register;
+
+namespace Skills.API.Controllers;
+
+[ApiController]
+[Route("api/users")]
+public class UsersController(IMediator mediator) : ControllerBase
+{
+    private readonly IMediator mediator = mediator;
+
+
+    [HttpPost]
+    public async Task<ActionResult<RegisterUserReponse>> RegisterUser(RegisterUserRequest request, CancellationToken cancellationToken)
+    {
+        var response = await mediator.Send(request, cancellationToken);
+        return Created("/api/users", response);
+    }
+}
