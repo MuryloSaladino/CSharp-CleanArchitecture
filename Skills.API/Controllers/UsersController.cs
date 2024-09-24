855341e4e959c9e5abe7cdf933d792a3a2097a2d
MuryloSaladino <murylosaladino@gmail.com>
2024-09-24 11:02:11 -0300

    login

--- Skills.API/Controllers/UsersController.cs
+++ Skills.API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Skills.Application.Features.Users.Login;
 using Skills.Application.Features.Users.Register;
 
 namespace Skills.API.Controllers;
@@ -17,5 +18,12 @@ public class UsersController(IMediator mediator) : ControllerBase
     {
         var response = await mediator.Send(request, cancellationToken);
         return Created("/api/users", response);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<LoginUserResponse>> LoginUser(LoginUserRequest request, CancellationToken cancellationToken)
+    {
+        var response = await mediator.Send(request, cancellationToken);
+        return Ok(response);
     }
 }
--- Skills.Application/Features/Users/Login/LoginUserHandler.cs
+++ Skills.Application/Features/Users/Login/LoginUserHandler.cs
@@ -1,11 +1,28 @@
 using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Skills.Application.Common.Exceptions;
+using Skills.Application.Repository.UserRepository;
+using Skills.Domain.Entities;
 
 namespace Skills.Application.Features.Users.Login;
 
-public sealed class LoginUserHandler : IRequestHandler<LoginUserRequest, LoginUserResponse>
+public sealed class LoginUserHandler(
+    IUserRepository userRepository,
+    PasswordHasher<User> passwordHasher
+) : IRequestHandler<LoginUserRequest, LoginUserResponse>
 {
-    public Task<LoginUserResponse> Handle(LoginUserRequest request, CancellationToken cancellationToken)
+    private readonly IUserRepository userRepository = userRepository;
+    private readonly PasswordHasher<User> hasher = passwordHasher;
+
+
+    public async Task<LoginUserResponse> Handle(LoginUserRequest request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var user = await userRepository.GetByUsername(request.Username, cancellationToken)
+            ?? throw new AppException("User not found", 404);
+        
+        if(hasher.HashPassword(user, request.Password) != user.Password)
+            throw new AppException("Incorrect credentials", 401);
+        
+        return new LoginUserResponse("token muito brabo");
     }
 }
