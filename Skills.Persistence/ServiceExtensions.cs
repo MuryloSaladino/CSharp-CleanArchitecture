f4a1a84c0d7336c7ab7ae6313598b14f60d6b5fb
MuryloSaladino <murylosaladino@gmail.com>
2024-09-24 09:08:26 -0300

    dependencies injected

--- Skills.Persistence/ServiceExtensions.cs
+++ Skills.Persistence/ServiceExtensions.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Skills.Application.Repository;
+using Skills.Application.Repository.SkillRepository;
+using Skills.Application.Repository.UserRepository;
+using Skills.Persistence.Context;
+using Skills.Persistence.Repository;
+using Skills.Persistence.Repository.Skills;
+using Skills.Persistence.Repository.Users;
 
 namespace Skills.Persistence;
 
@@ -8,5 +16,9 @@ public static class ServiceExtensions
     public static void ConfigureExtensions(this IServiceCollection services, IConfiguration configuration)
     {
         var connection = configuration.GetConnectionString("SkillsContext");
+        services.AddDbContext<SkillsContext>(opt => opt.UseSqlServer(connection));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ISkillRepository, SkillRepository>();
     }
 }
