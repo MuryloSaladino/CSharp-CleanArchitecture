3c55f614a9f3a3e9c963e8090c8872da064fd0de
MuryloSaladino <murylosaladino@gmail.com>
2025-03-04 15:38:08 -0300

    feat: password encrypter service

---
+++ Skills.Application/Services/PasswordEncrypterService.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Skills.Domain.Contracts;
+using Skills.Domain.Entities;
+
+namespace Skills.Application.Services;
+
+public class PasswordEncrypterService : IPasswordEncrypter
+{
+    private readonly PasswordHasher<User> hasher = new();
+
+    public string Hash(User user)
+        => hasher.HashPassword(user, user.Password);
+    
+
+    public bool Matches(User user, string password)
+    {
+        var result = hasher.VerifyHashedPassword(user, user.Password, password);
+        return result == PasswordVerificationResult.Success;
+    }
+}
