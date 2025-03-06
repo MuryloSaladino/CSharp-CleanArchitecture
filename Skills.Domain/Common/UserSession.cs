344cc06f2017a7c22c1c8944e0bb077cbe61540f
MuryloSaladino <murylosaladino@gmail.com>
2025-03-06 16:29:37 -0300

    feat: admin attribute for users

--- Skills.Domain/Common/UserSession.cs
+++ Skills.Domain/Common/UserSession.cs
@@ -1,7 +1,8 @@
 namespace Skills.Domain.Common;
 
-public class UserSession(string username, string? id)
+public class UserSession(string username, string? id, bool isAdmin = false)
 {
-    public string Username { get; set; } = username;
     public Guid? Id { get; set; } = Guid.TryParse(id, out var parsedId) ? parsedId : null;
+    public string Username { get; set; } = username;
+    public bool IsAdmin { get; set; } = isAdmin;
 }
--- Skills.Domain/Entities/User.cs
+++ Skills.Domain/Entities/User.cs
@@ -6,5 +6,6 @@ public class User : BaseEntity
 {
     public required string Username { get; set; }
     public required string Password { get; set; }
+    public required bool IsAdmin { get; set; }
     public List<Skill> Skills { get; set; } = [];
 }
