0f8a2321e0e690bf812c9e3408b5855b81b8686e
MuryloSaladino <murylosaladino@gmail.com>
2025-03-06 16:31:16 -0300

    feat: admin attribute migration

--- Skills.Persistence/Migrations/20250306172846_AdminUserMigration.Designer.cs
+++ Skills.Persistence/Migrations/20250306172846_AdminUserMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Skills.Persistence.Context;
@@ -11,9 +12,11 @@ using Skills.Persistence.Context;
 namespace Skills.Persistence.Migrations
 {
     [DbContext(typeof(SkillsContext))]
-    partial class SkillsContextModelSnapshot : ModelSnapshot
+    [Migration("20250306172846_AdminUserMigration")]
+    partial class AdminUserMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
