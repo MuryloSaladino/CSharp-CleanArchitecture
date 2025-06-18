876aee5ef6b017feb482dc30961d88b1f3ff286b
MuryloSaladino <murylosaladino@gmail.com>
2025-06-18 18:12:27 -0300

    feat: error handler

---
+++ Skills.API/Pipeline/Handlers/ErrorHandler.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics;
+using Skills.Domain.Common.Exceptions;
+
+namespace Skills.API.Pipeline.Handlers;
+
+public static class ErrorHandlerExtensions
+{
+    public static void UseErrorHandler(this IApplicationBuilder app) =>
+        app.UseExceptionHandler(error =>
+        {
+            error.Run(async context =>
+            {
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if(contextFeature is null) return;
+
+                var statusCode = contextFeature.Error switch
+                {
+                    AppException appError => appError.StatusCode,
+                    _ => ExceptionCode.InternalServerError
+                };
+                var message = contextFeature.Error switch
+                {
+                    AppException appError => appError.Message,
+                    _ => "Internal Server Error"
+                };
+
+                context.Response.Headers.Append("Access-Control-Allow-Origin", "*");
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)statusCode;
+
+                var errorResponse = new { statusCode, message };
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+            });
+        });
+}
