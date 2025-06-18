dc91e32797ea674f21e22d3f288eb1078da4f7cc
MuryloSaladino <murylosaladino@gmail.com>
2025-06-18 18:08:50 -0300

    feat: entities mappings for ef core

---
+++ Skills.Persistence/Mappings/UserSkills.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Skills.Domain.Entities;
+
+namespace Skills.Persistence.Mappings;
+
+public class UserSkillsConfiguration : IEntityTypeConfiguration<UserSkill>
+{
+    public void Configure(EntityTypeBuilder<UserSkill> builder)
+    {
+        builder.ToTable("user_skills");
+
+        builder.HasKey(us => new { us.UserId, us.SkillId });
+
+        builder.Property(us => us.UserId)
+            .HasColumnName("user_id")
+            .IsRequired();
+
+        builder.Property(us => us.SkillId)
+            .HasColumnName("skill_id")
+            .IsRequired();
+        builder.HasOne(us => us.Skill)
+            .WithMany()
+            .HasForeignKey(us => us.SkillId);
+
+        builder.Property(us => us.Level)
+            .HasColumnName("level")
+            .HasColumnType("smallint")
+            .IsRequired();
+
+        builder.Property(us => us.CreatedAt)
+            .HasColumnName("created_at")
+            .HasColumnType("timestamptz")
+            .IsRequired();
+    }
+}
