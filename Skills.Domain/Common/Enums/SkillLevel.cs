bd4987829262be2e89b7e3c5424abbe18920bcf1
MuryloSaladino <murylosaladino@gmail.com>
2025-06-18 18:03:30 -0300

    feat: skill level enum

---
+++ Skills.Domain/Common/Enums/SkillLevel.cs
@@ -0,0 +1,15 @@
+namespace Skills.Domain.Common.Enums;
+
+public enum SkillLevel
+{
+    Novice,
+    Beginner,
+    Apprentice,
+    Intermediate,
+    Skilled,
+    Proficient,
+    Expert,
+    Master,
+    Grandmaster,
+    Legend,
+}
