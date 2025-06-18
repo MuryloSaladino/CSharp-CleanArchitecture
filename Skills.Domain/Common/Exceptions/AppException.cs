4fd0559aea7a6a26917517d1d010e01c50775de7
MuryloSaladino <murylosaladino@gmail.com>
2025-06-18 18:03:17 -0300

    feat: domain exception module

---
+++ Skills.Domain/Common/Exceptions/AppException.cs
@@ -0,0 +1,7 @@
+namespace Skills.Domain.Common.Exceptions;
+
+public class AppException(ExceptionCode statusCode, string message, string? details = null) : Exception(message)
+{
+    public ExceptionCode StatusCode { get; set; } = statusCode;
+    public string? Details = details;
+}
---
+++ Skills.Domain/Common/Exceptions/ExceptionCode.cs
@@ -0,0 +1,14 @@
+namespace Skills.Domain.Common.Exceptions;
+
+public enum ExceptionCode
+{
+    BadRequest = 400,
+    Unauthorized = 401,
+    Forbidden = 403,
+    NotFound = 404,
+    Conflict = 409,
+    ImATeapot = 418,
+    InternalServerError = 500,
+    NotImplemented = 501,
+    BadGateway = 502,
+}
