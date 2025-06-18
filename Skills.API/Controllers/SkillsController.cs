1ee0be5d0a0d3fe1293429df6c9648d46d05d273
MuryloSaladino <murylosaladino@gmail.com>
2025-06-18 18:17:08 -0300

    feat: skills controller

--- Skills.API/Controllers/SkillsController.cs
+++ Skills.API/Controllers/SkillsController.cs
@@ -1,41 +1,11 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Skills.API.Middlewares.Authenticate;
-using Skills.Application.Features.Skills.Create;
-using Skills.Application.Features.Skills.Delete;
-using Skills.Application.Features.Skills.Edit;
+using Skills.API.Constants;
 
 namespace Skills.API.Controllers;
 
-[ApiController]
-[Route("/skills")]
-[Authenticate]
+[ApiController, Route(APIRoutes.Skills)]
 public class SkillsController(IMediator mediator) : ControllerBase
 {
-    private readonly IMediator mediator = mediator;
-
-    [HttpPost]
-    public async Task<ActionResult<CreateSkillResponse>> CreateSkill(
-        CreateSkillRequest request, CancellationToken cancellationToken)
-    {
-        var response = await mediator.Send(request, cancellationToken);
-        return Created("/skills", response);
-    }
-
-    [HttpPut, Route("{id}")]
-    public async Task<ActionResult<EditSkillResponse>> EditSkill(
-        [FromRoute] string id, EditSkillRequest request, CancellationToken cancellationToken)
-    {
-        request.Id = id;
-        var response = await mediator.Send(request, cancellationToken);
-        return Ok(response);
-    }
-
-    [HttpDelete, Route("{id}")]
-    public async Task<ActionResult> DeleteSkill(
-        [FromRoute] string id, CancellationToken cancellationToken)
-    {
-        await mediator.Send(new DeleteSkillRequest(id), cancellationToken);
-        return NoContent();
-    }
+    
 }
