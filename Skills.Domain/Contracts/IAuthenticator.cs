f1d894fabee12b3b07a765f600fbc91a1c4cb016
MuryloSaladino <murylosaladino@gmail.com>
2025-06-23 18:00:52 -0300

    chore: removed old domain rules

--- Skills.Domain/Contracts/IAuthenticator.cs
+++
@@ -1,9 +0,0 @@
-using Skills.Domain.Entities;
-
-namespace Skills.Domain.Contracts;
-
-public interface IAuthenticator
-{
-    string GenerateToken(User user);
-    Task<User> ExtractUserFromToken(string token, CancellationToken cancellationToken);
-}
--- Skills.Domain/Contracts/IPasswordEncrypter.cs
+++
@@ -1,9 +0,0 @@
-using Skills.Domain.Entities;
-
-namespace Skills.Domain.Contracts;
-
-public interface IPasswordEncrypter
-{
-    string Hash(User user);
-    bool Matches(User user, string password);
-}
