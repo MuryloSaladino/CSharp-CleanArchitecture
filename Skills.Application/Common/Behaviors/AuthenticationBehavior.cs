5c131e12d2c0fe41ba08f8aadbeed927a239cc26
MuryloSaladino <murylosaladino@gmail.com>
2025-06-23 18:01:44 -0300

    feat: request authentication behavior with tokens

---
+++ Skills.Application/Common/Behaviors/AuthenticationBehavior.cs
@@ -0,0 +1,65 @@
+using System.Reflection;
+using MediatR;
+using Skills.Domain.Identity;
+using Skills.Domain.Exceptions;
+using Skills.Domain.Repository.RefreshTokens;
+using Skills.Domain.Repository;
+
+namespace Skills.Application.Common.Behaviors;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+public class AuthenticateAttribute : Attribute
+{
+    public bool AdminOnly { get; init; }
+}
+
+public class AuthenticationBehavior<TRequest, TResponse>(
+    IRefreshTokensRepository refreshTokensRepository,
+    ITokenAuthenticator tokenAuthenticator,
+    ISessionContext session,
+    IUnitOfWork unitOfWork
+) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var authAttribute = request.GetType().GetCustomAttribute<AuthenticateAttribute>();
+
+        if (authAttribute is not null)
+        {
+            // Access token authentication
+            if (session.AccessToken is string accessToken)
+            {
+                var tokenPayload = tokenAuthenticator.Extract(accessToken);
+
+                if (authAttribute.AdminOnly && !tokenPayload.IsAdmin)
+                    throw new AppException(ExceptionCode.Forbidden, ExceptionMessages.Forbidden.Admin);
+
+                session.UserId = tokenPayload.UserId;
+            }
+            // Refresh token authentication
+            else
+            {
+                if (session.RefreshToken is null)
+                    throw new AppException(ExceptionCode.Unauthorized, ExceptionMessages.Unauthorized.Default);
+
+                var refreshTokenEntity = await refreshTokensRepository.Find(session.RefreshToken, cancellationToken)
+                    ?? throw new AppException(ExceptionCode.Unauthorized, ExceptionMessages.Unauthorized.RefreshToken);
+
+                if (refreshTokenEntity.ExpiresAt < DateTime.UtcNow)
+                    throw new AppException(ExceptionCode.Unauthorized, ExceptionMessages.Unauthorized.RefreshToken);
+
+                if (authAttribute.AdminOnly && !refreshTokenEntity.User.IsAdmin)
+                    throw new AppException(ExceptionCode.Forbidden, ExceptionMessages.Forbidden.Admin);
+
+                refreshTokenEntity.Rotate();
+                await unitOfWork.Save(cancellationToken);
+
+                session.RefreshToken = refreshTokenEntity.Value;
+                session.AccessToken = tokenAuthenticator.GenerateToken(TokenPayload.FromUser(refreshTokenEntity.User));
+                session.UserId = refreshTokenEntity.UserId;
+            }
+        }
+        return await next();
+    }
+}
