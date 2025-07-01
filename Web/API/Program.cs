fd782578db57a59052df62ee0712f83472cff9a8
MuryloSaladino <murylosaladino@gmail.com>
2025-07-01 20:53:37 -0300

    feat: app build and startup

---
+++ Web/API/Program.cs
@@ -0,0 +1,51 @@
+using System.Text.Json.Serialization;
+using dotenv.net;
+using Web.API.Pipeline.Cors;
+using Web.API.Pipeline.Handlers;
+using Web.API.Pipeline.Middlewares;
+using Application;
+using Infrastructure.Identity;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Context;
+using Infrastructure.Persistence.Seeding;
+
+DotEnv.Load(options: new DotEnvOptions(envFilePaths: ["../.env"]));
+
+var builder = WebApplication.CreateBuilder(args);
+
+// LAYERS CONFIG
+builder.Services.ConfigureApplication();
+builder.Services.ConfigurePersistence();
+builder.Services.ConfigureIdentity();
+
+// CORS
+builder.Services.ConfigureCorsPolicy();
+
+// CONTROLLERS AND OPTIONS
+builder.Services.AddControllers().AddJsonOptions(op =>
+{
+    op.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    op.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+// SWAGGER
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+var serviceScope = app.Services.CreateScope();
+var dataContext = serviceScope.ServiceProvider.GetService<SkillsContext>()
+    ?? throw new InvalidOperationException("Failed to resolve SkillsContext from service provider.");
+
+dataContext.Database.EnsureCreated();
+await dataContext.SeedAdmin();
+
+app.UseMiddleware<SessionMiddleware>();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
+app.UseErrorHandler();
+app.MapControllers();
+app.Run();
