5de71164eb0f5c8a1eb43fb3ec863402ab08dd3c
MuryloSaladino <murylosaladino@gmail.com>
2025-07-01 20:50:07 -0300

    feat: cookies and api route constants

---
+++ Web/API/Constants/APIRoutes.cs
@@ -0,0 +1,8 @@
+namespace Web.API.Constants;
+
+public static class APIRoutes
+{
+    public const string Auth = "api/auth";
+    public const string Users = "api/users";
+    public const string Skills = "api/users";
+}
---
+++ Web/API/Constants/Cookies.cs
@@ -0,0 +1,7 @@
+namespace Web.API.Constants;
+
+public static class Cookies
+{
+    public const string AccessToken = "accessToken";
+    public const string RefreshToken = "refreshToken";
+}
