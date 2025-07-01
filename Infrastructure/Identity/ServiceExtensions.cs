510fb9de20133fe82c29d08ab42fb09a07c8cd11
MuryloSaladino <murylosaladino@gmail.com>
2025-07-01 20:46:54 -0300

    feat: identity module implementation

---
+++ Infrastructure/Identity/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+using Domain.Identity;
+using Infrastructure.Identity.Context;
+using Infrastructure.Identity.Services;
+
+namespace Infrastructure.Identity;
+
+public static class IdentityConfiguration
+{
+    public static void ConfigureIdentity(this IServiceCollection services)
+    {
+        services.AddScoped<IPasswordEncrypter, PasswordEncrypter>();
+        services.AddScoped<ITokenAuthenticator, TokenAuthenticator>();
+        
+        services.AddScoped<ISessionContext, SessionContext>();
+    }
+}
